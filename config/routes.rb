58747acc39b313d2eac998dc91d0d713980854b2
Isabel Chan <isabelchan1994@gmail.com>
2018-04-05 15:16:51 +0800

    create controller - cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [:create, :index, :show, :new] do
+    resources :doses, only: [:new, :create, :delete]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
