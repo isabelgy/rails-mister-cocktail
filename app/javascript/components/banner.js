29e463485ed3ed9ff27cce903f01e41a5d3203e5
Dan Li <dl.phpf@gmail.com>
2018-04-06 13:53:35 +0800

    show page styled

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,9 +3,24 @@ import Typed from 'typed.js';
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
     strings: ["Create Your Own Cocktail"],
-    typeSpeed: 80,
+    typeSpeed: 100,
     loop: true
   });
 }
 
 export { loadDynamicBannerText };
+
+
+import swal from 'sweetalert';
+
+function bindSweetAlertButtonDemo() {
+  document.getElementById('sweet-alert-demo').addEventListener('click', () => {
+    swal({
+      title: "A nice alert",
+      text: "This is a great alert, isn't it?",
+      icon: "success"
+    })
+  });
+}
+
+export { bindSweetAlertButtonDemo };
